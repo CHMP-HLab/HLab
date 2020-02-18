42785244e59d639f53cc054240b4f7910401796c
mathieu <mathieu@lesbouys.net>
2020-02-18 17:57:23 +0100

    prevent wrong property naming

--- HLab.Notify.PropertyChanged/PropertyHelper.cs
+++ HLab.Notify.PropertyChanged/PropertyHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Reflection;
+using System.Text;
 using HLab.Base;
 
 namespace HLab.Notify.PropertyChanged
@@ -8,7 +9,16 @@ namespace HLab.Notify.PropertyChanged
     public class NotifyHelper
     {
         public static IEventHandlerService EventHandlerService { get; set; } = new EventHandlerService();
-        protected static string Name(string name) => (name.StartsWith("_"))?(name[1].ToString()).ToUpper() + name.Substring(2):name;
+        protected static string Name(string name)
+        {
+            if (!name.StartsWith("_") || name.Length <= 1) return name;
+
+            var n = new StringBuilder();
+            n.Append(name[1].ToString().ToUpper());
+            if (name.Length > 2) n.Append(name.Substring(2));
+            return n.ToString();
+        }
+
         protected static readonly MethodInfo SetParent = typeof(IChildObject).GetMethod("SetParent", new Type[] { typeof(object),typeof(Action<PropertyChangedEventArgs>) });
     }
 }
