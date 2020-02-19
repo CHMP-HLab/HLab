8f52f30c2c9e495cb22adc6bea5e933e9064203f
mathieu <mathieu@lesbouys.net>
2020-02-19 12:17:53 +0100

    Rebase DetailView

---
+++ HLab.Base.Wpf/Converters/MarginRatioConverter.cs
@@ -0,0 +1,62 @@
+﻿/*
+  HLab.Mvvm
+  Copyright (c) 2017 Mathieu GRENET.  All right reserved.
+
+  This file is part of HLab.Mvvm.
+
+    HLab.Mvvm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HLab.Mvvm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MouseControl.  If not, see <http://www.gnu.org/licenses/>.
+
+	  mailto:mathieu@mgth.fr
+	  http://www.mgth.fr
+*/
+
+using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HLab.Base.Converters
+{
+    public class MarginRatioConverter : IValueConverter
+    {
+        public Rect PhysicalRect { get; set; }
+        public FrameworkElement FrameworkElement { get; set; }
+
+        private double Ratio => Math.Min(
+            FrameworkElement.ActualWidth / PhysicalRect.Width,
+            FrameworkElement.ActualHeight / PhysicalRect.Height
+        );
+            public double PhysicalToUiX(double x)
+                => (x - PhysicalRect.Left) * Ratio
+                   + (FrameworkElement.ActualWidth
+                      - PhysicalRect.Width * Ratio) / 2;
+            public double PhysicalToUiY(double y)
+                => (y - PhysicalRect.Top) * Ratio
+                   + (FrameworkElement.ActualHeight
+                      - PhysicalRect.Height * Ratio) / 2;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var rect = (Rect) value;
+
+            return new Thickness(PhysicalToUiX(rect.X), PhysicalToUiY(rect.Y),0,0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((GridLength)value).Value / Ratio;
+        }
+    }
+
+}
--- HLab.Mvvm.Wpf/Views/YesNoView.xaml.cs
+++ HLab.Mvvm.Wpf/Views/YesNoView.xaml.cs
@@ -1,27 +1,30 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using HLab.Base;
 
 namespace HLab.Mvvm.Views
 {
+    using H = DependencyHelper<YesNoView>;
+
     /// <summary>
     /// Logique d'interaction pour YesNoView.xaml
     /// </summary>
     public partial class YesNoView : UserControl
     {
+
         public YesNoView()
         {
             InitializeComponent();
         }
 
-        class H : DependencyHelper<YesNoView> { }
 
         public static DependencyProperty ValueProperty = H.Property<bool?>()
             .OnChange((s, a) =>
             {
-                    s.ButtonYes.IsChecked = a.NewValue;
-                    s.ButtonNo.IsChecked = !a.NewValue;
-            })
+                s.SetValue(a.NewValue);
+
+            }).BindsTwoWayByDefault
             .Register();
 
         public static readonly DependencyProperty IsReadOnlyProperty =
@@ -41,15 +44,42 @@ namespace HLab.Mvvm.Views
             set => SetValue(IsReadOnlyProperty, value);
         }
 
+        private void SetValue(bool? value)
+        {
+            ButtonYes.IsChecked = value == true;
+            ButtonNo.IsChecked = value == false;
+        }
+
         private void Button_OnChecked(object sender, RoutedEventArgs e)
         {
+            if (IsReadOnly)
+            {
+                SetValue(Value);
+                return;
+            }
             if (ReferenceEquals(sender, ButtonYes)) Value = true;
             if (ReferenceEquals(sender, ButtonNo)) Value = false;
         }
         private void SetReadOnly()
         {
-            ButtonNo.IsEnabled = !IsReadOnly;
-            ButtonYes.IsEnabled = !IsReadOnly;
+            //ButtonNo.IsEnabled = !IsReadOnly;
+            //ButtonYes.IsEnabled = !IsReadOnly;
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (IsReadOnly)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsReadOnly)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
