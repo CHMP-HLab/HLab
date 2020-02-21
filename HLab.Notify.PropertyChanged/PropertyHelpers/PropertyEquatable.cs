415b6274edd3ecd9da6795d788ea8b72faae578d
mathieu <mathieu@lesbouys.net>
2020-02-21 18:29:31 +0100

    Allow array comparaison

--- HLab.Notify.PropertyChanged/PropertyHelpers/PropertyEquatable.cs
+++ HLab.Notify.PropertyChanged/PropertyHelpers/PropertyEquatable.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.Collections;
 using System.ComponentModel;
 
 namespace HLab.Notify.PropertyChanged
 {
+    public class PropertyStructuralEquatable<T> : IPropertyValue<T>
+        where T : IStructuralEquatable
+    {
+        private T _value;
+        public T Get() => _value;
+        public bool Set(Func<object, T> setter)
+            => Set(setter(_holder.Parent));
+
+        public bool Set(T value)
+        {
+            if (_value == null)
+            {
+                if(value == null) return false;
+                _value = value;
+                _holder.OnPropertyChanged();
+                return true;
+            }
+
+            if (!_value.Equals(value, StructuralComparisons.StructuralEqualityComparer))
+            {
+                _value = value;
+                _holder.OnPropertyChanged();
+                return true;
+            }
+            else return false;
+        }
+
+        private readonly PropertyHolder<T> _holder;
+        public PropertyStructuralEquatable(PropertyHolder<T> holder)
+        {
+            _holder = holder;
+        }
+    }
     public class PropertyEquatable<T> : IPropertyValue<IEquatable<T>>
     {
         private IEquatable<T> _value;
--- HLab.Notify.PropertyChanged/PropertyHelpers/PropertyValueLazy.cs
+++ HLab.Notify.PropertyChanged/PropertyHelpers/PropertyValueLazy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
@@ -11,66 +12,92 @@ namespace HLab.Notify.PropertyChanged
         private Func<object, T> _getter;
         private readonly PropertyHolder<T> _holder;
 
-        private static readonly Func<PropertyHolder<T>, IPropertyValue<T>> GetPropertyValue;
+        private static readonly Func<PropertyHolder<T>, IPropertyValue<T>> _getPropertyValue;
 
         static PropertyValueLazy()
         {
-            var t = typeof(T);
+            var propertyType = typeof(T);
+            if (propertyType.IsPrimitive)
+            {
+                if (propertyType == typeof(int))
+                {
+                    _getPropertyValue = e => (IPropertyValue<T>)new PropertyInt(e as PropertyHolder<int>);
+                    return;
+                }
+                if (propertyType == typeof(long))
+                {
+                    _getPropertyValue = e => (IPropertyValue<T>)new PropertyLong(e as PropertyHolder<long>);
+                    return;
+                }
 
-            if (t == typeof(int))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyInt(e as PropertyHolder<int>);
-            else if (t == typeof(long))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyLong(e as PropertyHolder<long>);
-            else if (t == typeof(float))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyFloat(e as PropertyHolder<float>);
-            else if (t == typeof(double))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyDouble(e as PropertyHolder<double>);
-            else if (t == typeof(bool))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyBool(e as PropertyHolder<bool>);
-            else if (t == typeof(string))
-                GetPropertyValue = e => (IPropertyValue<T>)new PropertyString(e as PropertyHolder<string>);
+                if (propertyType == typeof(float))
+                {
+                    _getPropertyValue = e => (IPropertyValue<T>)new PropertyFloat(e as PropertyHolder<float>);
+                    return;
+                }
+
+                if (propertyType == typeof(double))
+                {
+                    _getPropertyValue = e => (IPropertyValue<T>)new PropertyDouble(e as PropertyHolder<double>);
+                    return;
+                }
+
+                if (propertyType == typeof(bool))
+                {
+                    _getPropertyValue = e => (IPropertyValue<T>)new PropertyBool(e as PropertyHolder<bool>);
+                    return;
+                }
+            }
+
+            if (propertyType == typeof(string))
+            {
+                _getPropertyValue = e => (IPropertyValue<T>)new PropertyString(e as PropertyHolder<string>);
+                return;
+            }
+
+            Type makeType = null;
+            if (propertyType.IsInterface)
+                makeType = typeof(PropertyStruct<T>);//TODO maybe we can optimize
+            else if (propertyType.IsArray)
+                makeType = typeof(PropertyStructuralEquatable<>).MakeGenericType(propertyType);
+            else if (propertyType.IsClass)
+                makeType = typeof(PropertyObject<>).MakeGenericType(propertyType);
+            else if (propertyType.IsEnum && Enum.GetUnderlyingType(propertyType) == typeof(int))
+                makeType = typeof(PropertyEnum<>).MakeGenericType(propertyType);
+            else if (propertyType.IsValueType)
+                makeType = typeof(PropertyStruct<T>);//.MakeGenericType(propertyType);
             else
             {
-                var propertyType = typeof(T);
-                Type makeType = null;
-                if (propertyType.IsClass)
-                    makeType = typeof(PropertyObject<>).MakeGenericType(propertyType);
-                else if (propertyType.IsEnum && Enum.GetUnderlyingType(propertyType) == typeof(int))
-                    makeType = typeof(PropertyEnum<>).MakeGenericType(propertyType);
-                else
-                    makeType = typeof(PropertyStruct<>).MakeGenericType(propertyType);
+                throw new ArgumentException("Invalid type");
+            }
+            var argumentTypes = new[] { typeof(PropertyHolder<T>) };
 
-                var argumentTypes = new[] { typeof(PropertyHolder<T>) };
+            var constructor = makeType.GetConstructor(
+                BindingFlags.Instance | BindingFlags.Public,
+                null,
+                CallingConventions.HasThis,
+                argumentTypes,
+                new ParameterModifier[0]);
 
-                var constructor = makeType.GetConstructor(
-                    BindingFlags.Instance | BindingFlags.Public,
-                    null,
-                    CallingConventions.HasThis,
-                    argumentTypes,
-                    new ParameterModifier[0]);
-
-                var lamdaParameterExpressions = new[]
-                {
+            var lamdaParameterExpressions = new[]
+            {
                     Expression.Parameter(typeof(PropertyHolder<T>), "holder"),
                 };
 
-                var constructorParameterExpressions = lamdaParameterExpressions
-                    .Take(argumentTypes.Length)
-                    .ToArray();
+            var constructorParameterExpressions = lamdaParameterExpressions
+                .Take(argumentTypes.Length)
+                .ToArray();
 
-                var constructorCallExpression =
-                    Expression.New(constructor, constructorParameterExpressions);
-                var func = Expression
-                    .Lambda<Func<PropertyHolder<T>, IPropertyValue<T>>>(constructorCallExpression, lamdaParameterExpressions)
-                    .Compile();
+            var constructorCallExpression =
+                Expression.New(constructor, constructorParameterExpressions);
+            var func = Expression
+                .Lambda<Func<PropertyHolder<T>, IPropertyValue<T>>>(constructorCallExpression, lamdaParameterExpressions)
+                .Compile();
 
-                GetPropertyValue = func;
-
-            }
-
+            _getPropertyValue = func;
         }
 
-        private IPropertyValue<T> GetProperty() => GetPropertyValue(_holder);
+        private IPropertyValue<T> GetProperty() => _getPropertyValue(_holder);
 
         public T Get()
         {
