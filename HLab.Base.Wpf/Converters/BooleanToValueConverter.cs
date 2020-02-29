a4c185768e3b84177d16668029ac926e2132a35d
mgthieu <mathieu@lesbouys.net>
2020-02-29 11:57:59 +0100

    more generic converters

--- HLab.Base.Wpf/Converters/BooleanToValueConverter.cs
+++ HLab.Base.Wpf/Converters/BooleanToValueConverter.cs
@@ -5,17 +5,27 @@ using System.Windows.Media;
 
 namespace HLab.Base.Converters
 {
-    public class BooleanToStringConverter : BooleanToValueConverter<string> { }
-    public class BooleanToBrushConverter : BooleanToValueConverter<Brush> { }
-    public class BooleanToVisibilityConverter : BooleanToValueConverter<Visibility> { }
-    public class BooleanToObjectConverter : BooleanToValueConverter<object> { }
-    public class BooleanToBooleanConverter : BooleanToValueConverter<bool> { }
-
-    public class BooleanToValueConverter<T> : IValueConverter
+    public class ToStringConverter : GenericConverter<string> { }
+    public class ToBrushConverter : GenericConverter<Brush> { }
+    public class ToVisibilityConverter : GenericConverter<Visibility> 
     {
+        public ToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+            NullValue = Visibility.Collapsed;
+        }
+    }
+    public class ToObjectConverter : GenericConverter<object> { }
+    public class ToBooleanConverter : GenericConverter<bool> { }
+
+
+
+    public class GenericConverter<T> : IValueConverter
+    {
+        public T NullValue { get; set; }
         public T FalseValue { get; set; }
         public T TrueValue { get; set; }
-        public T NullValue { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, global::System.Globalization.CultureInfo culture)
         {
@@ -23,13 +33,21 @@ namespace HLab.Base.Converters
             {
                 return b ? TrueValue : FalseValue;
             }
+            if (value is string s)
+            {
+                return string.IsNullOrWhiteSpace(s) ? FalseValue : TrueValue;
+            }
+            if (value is int i)
+            {
+                return i==0 ? FalseValue : TrueValue;
+            }
 
             return NullValue;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, global::System.Globalization.CultureInfo culture)
         {
-            return value?.Equals(TrueValue) ?? false;
+            throw new NotImplementedException();
         }
     }
 }
