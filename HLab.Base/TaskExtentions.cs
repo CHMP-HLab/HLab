f6af64f326917795e694be088791d19f66e827ce
mathieu <mathieu@lesbouys.net>
2020-04-02 16:20:11 +0200

    Task with error handlers

---
+++ HLab.Base/TaskExtentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLab.Base
+{
+    public interface IErrorHandler
+    {
+        void HandleError(Exception ex);
+    }
+
+
+    public static class TaskUtilities
+    {
+#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
+        public static async void FireAndForgetSafeAsync(this Task task, IErrorHandler handler = null)
+#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                handler?.HandleError(ex);
+            }
+        }
+    }}
