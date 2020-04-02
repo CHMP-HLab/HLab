ebf66a4af6187e3bdb8a9909e490a51ed7bdfbd9
mathieu <mathieu@lesbouys.net>
2020-04-02 16:16:51 +0200

    Async Command

--- HLab.Notify.PropertyChanged/NotifyCommand.cs
+++ HLab.Notify.PropertyChanged/NotifyCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Net.Mime;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using HLab.Base;
 
 namespace HLab.Notify.PropertyChanged
 {
@@ -27,10 +30,30 @@ namespace HLab.Notify.PropertyChanged
     public abstract class NotifyCommand : ChildObject, ICommand
     {
         private Action<object> _execute = null;
+        private Func<object,Task> _executeAsync = null;
         private Func<object,bool> _canExecuteFunc = null;
         private bool _canExecute = true;
 
         public void SetAction(Action execute) => _execute = o => execute();
+
+        public void SetAction(Func<Task> execute) => _executeAsync = async  o =>
+        {
+                _executing = true;
+                CanExecuteChanged?.Invoke(this,new EventArgs());
+                await execute().ConfigureAwait(true);
+                _executing = false;
+                CanExecuteChanged?.Invoke(this,new EventArgs());
+        };
+        //{
+        //        _executing = true;
+        //        NotifyHelper.EventHandlerService.Invoke(CanExecuteChanged,this,new EventArgs());
+        //        await NotifyHelper.EventHandlerService.InvokeAsync(execute);
+        //        _executing = false;
+        //        NotifyHelper.EventHandlerService.Invoke(CanExecuteChanged,this,new EventArgs());
+        //};
+
+        private volatile bool _executing = false;
+
         public void SetAction(Action<object> execute) => _execute = execute;
         public void SetCanExecute(Func<bool> func)
         {
@@ -59,15 +82,29 @@ namespace HLab.Notify.PropertyChanged
 
         public bool CanExecute(object parameter=null)
         {
+            if (_executing) return false;
+
             if (_canExecuteFunc == null) return _canExecute;
             return _canExecuteFunc(parameter);
         }
 
-
-        public void Execute(object parameter=null)
+        async void ICommand.Execute(object parameter)
         {
-            _execute?.Invoke(parameter);
+            if (_executeAsync != null)
+                await _executeAsync(parameter).ConfigureAwait(true);
+            else
+                _execute?.Invoke(parameter);
         }
+
+
+        //public async void Execute(object parameter=null)
+        //{
+        //    if (_executeAsync != null)
+        //        //await _executeAsync(parameter).ConfigureAwait(true);
+        //        await _executeAsync(parameter);
+        //    else
+        //        _execute?.Invoke(parameter);
+        //}
 
         public event EventHandler CanExecuteChanged;
     }
