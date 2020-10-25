b4092f45609f08d8efc2e857e4463db0e745920b
mgth <mathieu@mgth.fr>
2020-10-25 02:12:48 +0200

    UserControlNotifier for forms

---
+++ HLab.Notify.Wpf/ControlNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using HLab.Notify.PropertyChanged;
+using HLab.Notify.PropertyChanged.NotifyParsers;
+
+namespace HLab.Notify.Wpf
+{
+    public class UserControlNotifier : UserControl, INotifyPropertyChanged
+    {
+
+        protected INotifyClassHelper Parser;
+
+        protected UserControlNotifier()
+        {
+            Parser = NotifyClassHelper.GetParserUninitialized(this);
+        }
+            
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add => Parser.AddHandler(value);
+            remove => Parser.RemoveHandler(value);
+        }
+    }
+}
