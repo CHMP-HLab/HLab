d1abc81bd760540bbff483275e48de1bbf62df2f
mgthieu <mathieu@lesbouys.net>
2021-02-07 11:06:33 +0100

    Schollview mousewheel aware

---
+++ HLab.Base.Wpf/ControlsEx/ScrollViewerEx.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HLab.Base.Wpf
+{
+    public class ScrollViewerEx : ScrollViewer
+    {
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            ScrollToVerticalOffset(VerticalOffset - e.Delta);
+            e.Handled = true;
+            base.OnPreviewMouseWheel(e);
+        }
+    }
+}
