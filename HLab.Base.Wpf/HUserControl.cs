639591f60a799722250f32ff40e5903d4ac853e2
mgthieu <mathieu@lesbouys.net>
2021-02-07 09:38:09 +0100

    versions update

---
+++ HLab.Base.Wpf/HUserControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HLab.Base.Wpf
+{
+    class HUserControl<T> : UserControl
+    where T : DependencyObject
+    {
+        class H : DependencyHelper<T> {}
+    }
+}
---
+++ HLab.Base.Wpf/TextboxExtentions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using HLab.Base;
+
+namespace HLab.Base.Wpf
+{
+    public static class TextboxExtentions
+    {
+        public static void ApplySymbols(this TextBox tb)
+        { 
+            var pos = tb.CaretIndex;
+            var v1 = tb.Text;
+            var v2 = v1.ApplySymbols();
+
+            tb.Text = v2;
+
+            try
+            {
+                tb.CaretIndex = pos - (v1.Length - v2.Length);
+            }
+            catch(IndexOutOfRangeException){}
+            catch(ArgumentOutOfRangeException){}
+        }
+    }
+}
