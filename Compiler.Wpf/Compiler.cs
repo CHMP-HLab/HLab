0a631904c32f6928541f9cb40d945a97b4f8e585
mgth <mathieu@mgth.fr>
2021-03-29 14:11:46 +0200

    Compiler more universal

--- Compiler.Wpf/Compiler.cs
+++ Compiler.Wpf/Compiler.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
-using System.Windows;
-using System.Windows.Controls;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
@@ -15,99 +13,56 @@ namespace Compiler.Wpf
 {
     public class Compiler
     {
-        public string SourceCode { get; set; }
-        public string CsMessage { get; private set; }
-        public object Module { get; private set; }
-
-        public bool Compile()
+        public static Assembly Compile(out string messages, params string[] sources)
         {
-            var sourceCode = SourceCode;
-
             using var peStream = new MemoryStream();
 
-            var result = GenerateCode(sourceCode).Emit(peStream);
+            var result = GenerateCode(sources).Emit(peStream);
 
             if (!result.Success)
             {
-                CsMessage = "Compilation failed.";
+                messages = "Compilation failed.";
 
                 var failures = result.Diagnostics.Where(diagnostic => diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
 
                 foreach (var diagnostic in failures)
                 {
-                    CsMessage += $"\n{diagnostic.Id}: {diagnostic.GetMessage()}";
+                    messages += $"\n{diagnostic.Id}: {diagnostic.GetMessage()}";
                 }
 
-                return false;
+                return null;
             }
 
-            CsMessage = "C# OK";
+            messages = "C# OK";
 
             peStream.Seek(0, SeekOrigin.Begin);
 
             var compiled = peStream.ToArray();
             var assemblyLoadContext = new AssemblyLoadContext("HLab.RuntimeCompiled", true);
 
-            Assembly assembly;
-            using (var asm = new MemoryStream(compiled))
-            {
-                assembly = assemblyLoadContext.LoadFromStream(asm);
-            }
-
-            Module = Activator.CreateInstance(assembly.GetTypes()[0]);
-
-            return true;
+            using var asm = new MemoryStream(compiled);
+            return assemblyLoadContext.LoadFromStream(asm);
         }
 
-        private static CSharpCompilation GenerateCode(string sourceCode)
+        private static CSharpCompilation GenerateCode(string[] sources)
         {
-            var codeString = SourceText.From(sourceCode);
-            var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp7_3);
+            List<SyntaxTree> parsedSyntaxTrees = new();
+            foreach (var source in sources)
+            {
+                var codeString = SourceText.From(source);
+                var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp7_3);
 
-            var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(codeString, options);
+                parsedSyntaxTrees.Add(SyntaxFactory.ParseSyntaxTree(codeString, options));
+            }
 
             var references = AppDomain.CurrentDomain.GetAssemblies()
                 .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
                 .Select(a => MetadataReference.CreateFromFile(a.Location))
-                .ToArray();
+                /*.ToArray()*/;
 
-            //var references = new MetadataReference[]
-            //{
-            //    //System.Runtime
-            //    MetadataReference.CreateFromFile(typeof(System.Runtime.AssemblyTargetedPatchBandAttribute).Assembly.Location),
-            //    //System.Private.CoreLib
-            //    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-            //    //PresentationCore
-            //    MetadataReference.CreateFromFile(typeof(UIElement).Assembly.Location),
-            //    //PresentationFramework
-            //    MetadataReference.CreateFromFile(typeof(TextBox).Assembly.Location),
-            //    //WindowBase
-            //    MetadataReference.CreateFromFile(typeof(DependencyObject).Assembly.Location),
-            //    //HLab.Base.Wpf
-            //    MetadataReference.CreateFromFile(typeof(TextBoxEx).Assembly.Location),
-            //    //HLab.Erp.Lims.Analysis.Module
-            //    MetadataReference.CreateFromFile(typeof(IFormTarget).Assembly.Location),
-            //    MetadataReference.CreateFromFile(typeof(IForm).Assembly.Location),
-            //    //System.ObjectModel
-            //    MetadataReference.CreateFromFile(typeof(INotifyPropertyChanged).Assembly.Location),
-            //    MetadataReference.CreateFromFile(typeof(INotifyPropertyChangedWithHelper).Assembly.Location),
-            //    //HLab.Notify.PropertyChanged
-            //    MetadataReference.CreateFromFile(typeof(NotifierBase).Assembly.Location),
-            //    //HLab.Notify.PropertyChanged
-            //    MetadataReference.CreateFromFile(typeof(UserControlNotifier).Assembly.Location),
-            //    //
-
-            //    MetadataReference.CreateFromFile(Assembly.GetExecutingAssembly().Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("System.Runtime").Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("netstandard").Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("System.Linq").Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("System.Core").Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("System.Xaml").Location),
-            //    MetadataReference.CreateFromFile(Assembly.Load("System.ComponentModel.Primitives").Location),
-            //};
 
             return CSharpCompilation.Create("HLab.RuntimeCompiled.dll",
-                new[] { parsedSyntaxTree },
+                parsedSyntaxTrees,
                 references: references,
                 options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
                     optimizationLevel: OptimizationLevel.Release,
