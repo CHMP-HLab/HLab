e3b17f7e071d1abfc4096378a3352545f3daed00
mgth <mathieu@mgth.fr>
2021-04-06 14:12:46 +0200

    LogicalTreeExtensions

---
+++ HLab.Base.Wpf/LogicalTreeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HLab.Base.Wpf
+{
+    public static class LogicalTreeExtensions
+    {
+        public static IEnumerable<T> FindLogicalChildren<T>(this FrameworkElement fe) where T : FrameworkElement
+        {
+            if (fe == null) yield break;
+            foreach (var child in LogicalTreeHelper.GetChildren(fe))
+            {
+                if (child is T c)
+                {
+                    yield return c;
+                }
+
+                if (child is not FrameworkElement e) continue;
+                foreach (var childOfChild in FindLogicalChildren<T>(e))
+                {
+                    yield return childOfChild;
+                }
+            }
+        }
+    }
+}
