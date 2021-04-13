572e4ac00b59006778f50a97cf515cc383d1ecbe
mgth <mathieu@mgth.fr>
2021-04-13 10:07:38 +0200

    Icon import

--- HLab.Application.Wpf/Icons/IconListViewModel.cs
+++ HLab.Application.Wpf/Icons/IconListViewModel.cs
@@ -1,6 +1,9 @@
-﻿using HLab.Erp.Base.Data;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using HLab.Erp.Base.Data;
 using HLab.Erp.Core.EntityLists;
 using HLab.Erp.Core.ListFilters;
+using HLab.Erp.Data;
 using HLab.Mvvm.Annotations;
 
 namespace HLab.Mvvm.Application.Wpf.Icons
@@ -31,6 +34,27 @@ namespace HLab.Mvvm.Application.Wpf.Icons
 
         public void ConfigureMvvmContext(IMvvmContext ctx)
         {
+        }
+
+        protected override async Task ImportAsync(IDataService data, Icon newValue)
+        {
+            var done = false;
+            var icons = data.FetchWhereAsync<Icon>(e => e.Path == newValue.Path);
+            await foreach (var icon in icons)
+            {
+                icon.SourceXaml = newValue.SourceXaml;
+                icon.SourceSvg = newValue.SourceSvg;
+                icon.Foreground = newValue.Foreground;
+                await data.UpdateAsync(icon, "SourceXaml", "SourceSvg", "Foreground");
+                done = true;
+            }
+
+            if (!done) await data.AddAsync<Icon>(icon =>
+            {
+                icon.SourceXaml = newValue.SourceXaml;
+                icon.SourceSvg = newValue.SourceSvg;
+                icon.Foreground = newValue.Foreground;
+            });
         }
     }
 }
