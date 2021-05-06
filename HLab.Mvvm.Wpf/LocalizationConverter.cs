f915ecee8538b806e4689e7bc153ddf93f4053fa
mgth <mathieu@mgth.fr>
2021-05-06 21:15:53 +0200

    Localisation converter review

--- HLab.Mvvm.Wpf/LocalizationConverter.cs
+++ HLab.Mvvm.Wpf/LocalizationConverter.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using HLab.Localization.Wpf.Lang;
 using HLab.Mvvm.Annotations;
 
 namespace HLab.Mvvm
 {
     public class LocalizationConverter : IValueConverter
     {
+        private static object LocalizeObject(object obj)
+        {
+            if (obj is string s)
+            {
+                return new Localize() {Id = s};
+            }
+
+            return obj;
+        }
+
+
         public object Convert(object value, Type targetType,
             object parameter, CultureInfo culture)
         {
-            if(value is ILocalizationService l)
-            {
-                return l.LocalizeAsync((string)parameter);
-            }
-            if (parameter is string s)
-                return s;
 
-            return "_localization_failed_";
+            return LocalizeObject(value);
         }
 
         public object ConvertBack(object value, Type targetType,
