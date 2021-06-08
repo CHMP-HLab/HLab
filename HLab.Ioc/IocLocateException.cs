ba3047ef1a65e2eb0ccbb027ce025ff07fd19c70
mgth <mathieu@mgth.fr>
2021-06-08 22:58:09 +0200

    rearange

---
+++ HLab.Ioc/IocLocateException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HLab.Ioc
+{
+    public class IocLocateException : Exception
+    {
+        public IocLocateException(string message) : base(message)
+        {
+
+        }
+        public IocLocateException(string message, Exception inner) : base(message,inner)
+        {
+
+        }
+    }
+
+
+}
---
+++ HLab.Ioc/SingletonLocator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace HLab.Ioc
+{
+    public static class SingletonLocator<T>
+    {
+        private static bool _singletonInitialized = false;
+        private static object _lock = new object();
+
+        public static void SetInstance(T instance)
+        {
+            Locator<T>.SetFactoryExpression(Expression.Constant(instance));
+        }
+        public static void Set<T1>() where T1 : T, new()
+        {
+            var singleton = new T1();
+            SetInstance(singleton);
+            Locator.Enqueue(() =>
+            {
+                var injectExpression = Locator<T1>.InjectExpression();
+                injectExpression.Compile()(singleton);
+            });
+        }
+
+        public static void SetFactory(Expression<Func<T>> factory) => Locator<T>.SetFactory(() =>
+       {
+           Monitor.Enter(_lock);
+           try
+           {
+               if (_singletonInitialized) return Locator<T>.Locate();
+               var singleton = factory.Compile()();
+
+               var lambda = Expression.Lambda<Func<T>>(Expression.Constant(singleton));
+
+               Locator<T>.SetFactoryExpression(lambda);
+
+               _singletonInitialized = true;
+               return singleton;
+           }
+           finally
+           {
+               Monitor.Exit(_lock);
+           }
+       });
+    }
+
+
+}
